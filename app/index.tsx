6f54f92c79f7e4dd08dfd7757bdba079b6cbbbe1
Andre Luiz Thiel <43017985+andrethiel@users.noreply.github.com>
2024-12-06 11:14:29 -0300

    inicio

---
+++ app/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+// import { Container } from './styles';
+
+const Index: React.FC = () => {
+  return (
+    <View>
+      <Text>inicio</Text>
+    </View>
+  );
+};
+
+export default Index;
